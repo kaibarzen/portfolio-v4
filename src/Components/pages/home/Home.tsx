d9ddb3d2a7d486fb8fbaea8fcd9da5650463764d
Kai Barzen <k.barzen@volksfreund.de>
2021-03-25 17:15:29 +0100

    intro page

--- src/Components/pages/home/Home.tsx
+++ src/Components/pages/home/Home.tsx
@@ -6,6 +6,7 @@ const Home = () =>
     return (
         <div>
             <Intro/>
+	        <Intro/>
         </div>
     );
 };
--- src/Components/pages/home/intro/Intro.tsx
+++ src/Components/pages/home/intro/Intro.tsx
@@ -1,12 +1,31 @@
 import React from 'react';
+import './intro.sass';
+import person from "../../../../resources/media/person.svg"
 
 const Intro = () =>
 {
-    return (
-        <div>
-            INTROOOOOO
-        </div>
-    );
+	return (
+		<div className={'intro'}>
+			<div className={"person"} style={{backgroundImage:`url(${person})`}}>
+				<div className={"container"}>
+					<div>
+						<h1>
+							Hi, I'm Kai
+						</h1>
+
+						<p>
+							I'm a fullstack web developer which loves working with react and aws.
+						</p>
+					</div>
+
+					<div>
+						View Skills | View Projects | Contact
+					</div>
+
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default Intro;
