8a263be2e0bf32f9d9ff474d1f4a37797c3461d2
Kai Barzen <k.barzen@volksfreund.de>
2021-03-29 11:52:20 +0200

    content changes

--- src/Components/pages/home/projects/Projects.tsx
+++ src/Components/pages/home/projects/Projects.tsx
@@ -20,26 +20,25 @@ const Projects = () =>
 				<Project
 					title={'Dungeon Generator'}
 					image={dungeon}
-					tags={["React", "test"]}
+					tags={["React", "Typescript", "MobX"]}
 					gradient={[]}
-					source={"SRC"}
-					href={"asffasf"}
+					source={"https://github.com/kaibarzen/lf_dungeon"}
+					href={"https://dungeon.kaibarzen.cloud/"}
 				>
-					asasffas
-					afsfasfsa
-					fasfsa
+					Dungeon Generator is a minimalistic dungeon drawer for virtual tabletop games.
+					It's based around the HTML canvas object and includes many adv fts like asjkasfjn
 				</Project>
 			</div>
 			<Project
-				title={'Wahlen'}
+				title={'Rhineland-Palatinate Elections'}
 				image={wahlen}
-				tags={["React"]}
+				tags={["Typescript", "React", "AWS", "Amplify", "Lambda", "ApiGateway", "MongoDB"]}
 				reversed={true}
 				gradient={[]}
+				href={"https://wahlen.volksfreund.cloud/"}
 			>
-				asasffas
-				afsfasfsa
-				fasfsa
+				Election website made for the 2021 election in Rhineland-Palatinate.
+				Over the timespan of 24 hours the website got served 95,000 times with 135,000 successful api calls.
 			</Project>
 
 			<ProjectPortfolio
@@ -47,7 +46,9 @@ const Projects = () =>
 				tags={["React", "Typescript", "Sass"]}
 				gradient={["#c0392b", "#8e44ad"]}
 			>
-				This is listed so you can view the source code here ! :)
+				This is listed here so you view the sourcecode of my website.
+				<br/>
+				Also on the right hand is a scrollable miniature version of this website :)
 			</ProjectPortfolio>
 		</div>
 	);
